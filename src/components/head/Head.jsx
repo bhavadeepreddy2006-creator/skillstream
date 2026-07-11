60d08ee76606022dba2fd6ba1956c8c9c5792fce
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-11 23:47:15 +0530

    inital header

---
+++ src/components/head/Head.jsx
@@ -0,0 +1,8 @@
+import "./head.css";
+function Head(){
+    <div className="head">
+    <h1>SkillStream</h1>
+    <h3>A community-driven platform where skilled individuals share knowledge, real-world experiences, and practical solutions with learners</h3>
+    </div>
+}
+export default Head;
