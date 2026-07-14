85562bb95a537ac57747ef85c2cba9cec89709bf
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-14 23:58:17 +0530

    used navlink

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate, NavLink } from "react-router-dom";
 import "./Sidebar.css";
 
 import {
@@ -15,71 +16,60 @@ import {
 } from "react-icons/fa";
 
 function Sidebar() {
+  // const navigate = useNavigate();
+  // function toprofile(){
+  //   navigate("/myprofile")
+  // }
   return (
     <aside className="sidebar">
-
       <div className="sidebar-top">
-
         <h3 className="sidebar-title">Navigation</h3>
-
         <ul className="sidebar-menu">
-
           <li className="active">
             <FaHome className="icon" />
             <span>Dashboard</span>
           </li>
-
+          <NavLink to="/myprofile">
           <li>
             <FaUserCircle className="icon" />
             <span>My Profile</span>
           </li>
-
+          </NavLink>
           <li>
             <FaBookOpen className="icon" />
             <span>Knowledge Feed</span>
           </li>
-
           <li>
             <FaPenAlt className="icon" />
             <span>Create Post</span>
           </li>
-
           <li>
             <FaCompass className="icon" />
             <span>Explore</span>
           </li>
-
           <li>
             <FaUsers className="icon" />
             <span>Community</span>
           </li>
-
           <li>
             <FaBookmark className="icon" />
             <span>Saved Posts</span>
           </li>
-
           <li>
             <FaBell className="icon" />
             <span>Notifications</span>
           </li>
-
           <li>
             <FaChartBar className="icon" />
             <span>Analytics</span>
           </li>
-
           <li>
             <FaCog className="icon" />
             <span>Settings</span>
           </li>
-
         </ul>
-
       </div>
-
       <div className="sidebar-bottom">
-
         <button className="logout-btn">
           <FaSignOutAlt />
           Logout
