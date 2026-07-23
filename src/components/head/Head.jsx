850e06aac1a6caa640b4261d608e9979b8e28f25
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-23 10:05:59 +0530

    Initialized backend folder

--- src/components/head/Head.jsx
+++
@@ -1,8 +0,0 @@
-import "./head.css";
-function Head(){
-    <div className="head">
-    <h1>SkillStream</h1>
-    <h3>A community-driven platform where skilled individuals share knowledge, real-world experiences, and practical solutions with learners</h3>
-    </div>
-}
-export default Head;
